eed9e13e691894b7eba2dcb84b2c0f11f5c1df75
jiseok12 <103105145+jiseok12@users.noreply.github.com>
2022-07-29 09:48:40 +0900

    Add files via upload

---
+++ 1365/1365.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+	int a,b=1,c;
+	scanf("%d",&a);
+	c=a-2;
+	for(int i=1;i<=a;i++){
+		printf("*");
+	}
+	printf("\n");
+	for(int i=1;i<=a-2;i++){
+		printf("*");
+		for(int j=1;j<=a-2;j++){
+			if(j==b||j==c){
+				printf("*");
+			}
+			else{
+				printf(" ");
+			}
+		}
+		c--;
+		b++;
+		printf("*");
+		printf("\n");
+	}
+	for(int i=1;i<=a;i++){
+		printf("*");
+	}
+	return 0;
+}
